7da2c4121e7d51c462767a1c65100b753fb460ea
Behzad-Khokher <40473959+Behzad-Khokher@users.noreply.github.com>
2019-01-22 16:30:09 +0500

    Add files via upload

---
+++ mvc/app/views/inc/footer.php
@@ -0,0 +1,4 @@
+
+    <script src="<?php echo URLROOT; ?>/public/js/main.js" type="text/javascript"></script>
+</body>
+</html>
---
+++ mvc/app/views/pages/index.php
@@ -0,0 +1,5 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+
+<h1><?php echo $data['title']; ?></h1>
+<p>This the SimpleMVC Framework. Documentation Will be uploaded soon.</p>
+<?php require APPROOT . '/views/inc/footer.php'; ?>
